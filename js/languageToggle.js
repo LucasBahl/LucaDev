0a3efbcd7443fc4a228233119aaf1c56fa90a76e
Lucas <lucasbahl0@gmail.com>
2022-06-03 19:42:04 -0300

    Mobile version done

---
+++ js/languageToggle.js
@@ -0,0 +1,5 @@
+$('#language-toggle').click(function () {
+    var lang = $('html').attr('lang');
+    if (lang == 'en') $('html').attr('lang', 'es');
+    if (lang == 'es') $('html').attr('lang', 'en');
+  });
---
+++ js/loader.js
@@ -0,0 +1,7 @@
+let loader = document.getElementById("loader")
+let preloader = document.getElementById("preloader")
+
+window.addEventListener("load", function () {
+  loader.style.display = "none";
+  preloader.style.display = "none";
+});
